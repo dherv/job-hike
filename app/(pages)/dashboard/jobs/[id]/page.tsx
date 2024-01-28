e93cf7b3ce392c27276e62c20e5744920befefc3
dherv <damien.herv@gmail.com>
2024-01-28 13:49:41 +0900

    feat: add click handler on job card

---
+++ app/(pages)/dashboard/jobs/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { fetchCompanies } from "@/app/api/companies/services";
+import { fetchJobById } from "@/app/api/jobs/services";
+import { PageLayout } from "@/app/ui/layout/page-layout";
+import { JobEditForm } from "@/app/ui/pages/jobs/edit-form";
+import { notFound } from "next/navigation";
+
+// TODO: replace temporary page - add new view page
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [job, companies] = await Promise.all([
+        fetchJobById(id),
+        fetchCompanies(),
+    ]);
+
+    if (!job) {
+        return notFound();
+    }
+
+    return (
+        <PageLayout title="edit job">
+            <JobEditForm companies={companies} id={id} job={job} />
+        </PageLayout>
+    );
+}
