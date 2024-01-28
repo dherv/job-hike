ef21d87e5bce14be87446f437bf123a9c69e72f1
dherv <damien.herv@gmail.com>
2024-01-28 13:08:11 +0900

    feat(job-list): layout update spacing

--- app/ui/pages/jobs/card.tsx
+++ app/ui/pages/jobs/card.tsx
@@ -1,3 +1,4 @@
+"use client"
 import logo from "@/app/lib/logos/logoipsum-241.svg";
 import { JobWithCompany } from "@/app/lib/types";
 import { Card } from "@/app/ui/components/card";
@@ -7,36 +8,43 @@ import { CardCompanyName } from "./card-company-name";
 import { Pill } from "@/app/ui/components/pill";
 import { City } from "@/app/ui/components/city";
 import { CardJobName } from "./card-job-name";
+import { useRouter } from "next/navigation";
 
 export const JobCard = ({ job }: { job: JobWithCompany }) => {
+  const router = useRouter()
+
+  const handleClick = () => router.push(`jobs/${job.id}`)
+
   return (
-    <Card>
-      <div className="flex justify-between items-center">
-        <div className="flex gap-3">
-          <div className="flex-shrink-0">
-            {job.company?.logo ? (
-              <Image
-                width={48}
-                height={48}
-                className="h-12 w-12 rounded-full"
-                src={logo}
-                alt={job.company?.name}
-              />
-            ) : null}
-          </div>
-          <div>
-            <CardJobName>{job.title}</CardJobName>
-            <div className="flex items-center gap-4">
-              <CardCompanyName>{job.company?.name}</CardCompanyName>
-              <City city={job.company.city.name} />
+    <div className="hover:cursor-pointer" onClick={handleClick}>
+      <Card>
+        <div className="flex justify-between items-center">
+          <div className="flex gap-3">
+            <div className="flex-shrink-0">
+              {job.company?.logo ? (
+                <Image
+                  width={48}
+                  height={48}
+                  className="h-12 w-12 rounded-full"
+                  src={logo}
+                  alt={job.company?.name}
+                />
+              ) : null}
+            </div>
+            <div>
+              <CardJobName>{job.title}</CardJobName>
+              <div className="flex items-center gap-4">
+                <CardCompanyName>{job.company?.name}</CardCompanyName>
+                <City city={job.company.city.name} />
+              </div>
             </div>
           </div>
+          <div className="flex items-center gap-4">
+            <CardApplicationDate>{job.applicationDate}</CardApplicationDate>
+            <Pill>{job.applicationStatus}</Pill>
+          </div>
         </div>
-        <div className="flex items-center gap-4">
-          <CardApplicationDate>{job.applicationDate}</CardApplicationDate>
-          <Pill>{job.applicationStatus}</Pill>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 };
